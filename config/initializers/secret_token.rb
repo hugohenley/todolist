23ea84408a25210f8bff1a021f38d3d70a4aea11
Hugo Henley <hugohenley@gmail.com>
2012-12-30 16:47:41 -0200

    Criando o projeto usando Rails 3.2.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = '174c53d9a671aeb5b95e7ae883c4244c7c6816ba84629cc3e655689ab410baf256d9740852434a07c9675fc6e097abdff1126be5f9e32817063eb78fcceabb6e'
